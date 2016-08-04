a0244a14bbcaebae399fd460f02d62fc2eaf697a
HarrisLeow <leow_harris@hotmail.sg>
2016-08-04 23:57:26 +0800

    edit about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -16,11 +16,9 @@ class About extends React.Component {
                       <p>Full-Stack developer by day, sleeping by night. I have put pen to paper during design processes, worked with multiple developers, and also worn the entreprenuerial hat.</p>
                   </div>
                   <div className="col-lg-4">
-                      <p>I am an undergraduate at the National University of Singapore (NUS) and part of the NOC Silicon Valley program! Currently residing in San Francisco, CA. I am looking to further my career as a professional web developer.</p>
+                      <p>I am an undergraduate at the National University of Singapore (NUS) and part of the NOC Silicon Valley program! Currently residing in San Francisco, CA. Take some time to look at my work, credentials, and if you're interested in helping me make things happen, let's get in touch.</p>
                   </div>
-                  <div className="col-lg-4">
-                      <p>Take some time to look at my work, credentials, and if you're interested in helping me make things happen, let's get in touch.</p>
-                  </div>
+                  
                   <div className="col-lg-8 col-lg-offset-2 text-center">
                       <a href="#contact" className="btn btn-lg btn-outline">
                           <i className="fa fa-download"></i> Contact Me
